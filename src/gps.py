3cde26a1acb856767a862a83dc42831a81592850
Ignacy Kowalewski <ignacykowalewski314@gmail.com>
2025-10-06 23:14:08 +0200

    x

--- src/gps.py
+++ src/gps.py
@@ -1,7 +1,7 @@
 from collections import deque
 import time
 from serial import Serial
-from pynmeagps import NMEAReader
+from pynmeagps import NMEAReader, NMEA_MSGIDS
 from statistics import mean
 
 
@@ -17,9 +17,9 @@ def gps(stop_flag, gps_lon, gps_lat, gps_lock):
 
             # _, msgs = nmr.read()
 
-            for msg in nmr:
-                print(msg)
-                print()
+            for _, msg in nmr:
+                if msg.msgID == "GLL":
+                    print(msg)
 
             # if msg is not None and msg.lon is not None:
             #     lons.append(float(msg.lon))
