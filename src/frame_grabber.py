270da5c6667d1ec5858b1a704faccaebbb0f83e2
Ignacy Kowalewski <ignacykowalewski314@gmail.com>
2025-10-09 13:22:20 +0200

    x

--- src/frame_grabber.py
+++ src/frame_grabber.py
@@ -7,8 +7,16 @@ def frame_grabber(shm_name, shape, dtype, lock, stop_flag, new_frame):
     shm = shared_memory.SharedMemory(name=shm_name)
     frame_array = np.ndarray(shape, dtype=dtype, buffer=shm.buf)
 
-    video_path = "rtsp://admin:1plus2jest3@192.168.3.64:554/Streaming/Channels/103"
+    video_path = "rtsp://admin:1plus2jest3@192.168.3.64:554/Streaming/Channels/101"
     cap = cv2.VideoCapture(video_path)
+
+    frame_width = int(cap.get(3))  # Width of frame
+
+    frame_height = int(cap.get(4))  # Height of frame
+
+    fps = cap.get(cv2.CAP_PROP_FPS)  # Frames per second
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")  # Codec
+    out = cv2.VideoWriter("output.avi", fourcc, fps, (frame_width, frame_height))
 
     while True:
         if stop_flag.value == 1:
@@ -18,6 +26,11 @@ def frame_grabber(shm_name, shape, dtype, lock, stop_flag, new_frame):
         if not ret:
             continue
 
+        out.write(frame)
+
         with lock:
             np.copyto(frame_array, frame[::-1, ::-1])
             new_frame.value = 1
+
+    cap.release()
+    out.release()
